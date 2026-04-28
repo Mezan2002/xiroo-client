4bac615b5256b0ac9c9f9378ef8556bef7f99184
mezan2002 <mezanurr009@gmail.com>
2026-04-28 14:04:47 +0600

    feat: add interactive product comparison component with dual-image slider

--- components/pages/Home/ProductComparison.jsx
+++ components/pages/Home/ProductComparison.jsx
@@ -30,7 +30,7 @@ export default function ProductComparison() {
         {/* Background Image (Dark Variant) */}
         <div className="absolute inset-0 w-full h-full">
           <Image
-            src="/images/mannequin-2.png"
+            src="/images/compersion-1.jpg"
             alt="Dark Variant Product View"
             fill
             className="object-cover object-center pointer-events-none"
@@ -43,7 +43,7 @@ export default function ProductComparison() {
           style={{ clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` }}
         >
           <Image
-            src="/images/mannequin.png"
+            src="/images/compersion-2.jpg"
             alt="Light Variant Product View"
             fill
             className="object-cover object-center"
